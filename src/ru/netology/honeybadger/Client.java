b9ffb846c2fd28d208784535d622bcac3cbf1178
Dmitry Ushakov <ushakov-dp@mail.ru>
2022-07-12 16:08:01 +0700

    Non-Blocking

--- src/ru/netology/honeybadger/Client.java
+++ src/ru/netology/honeybadger/Client.java
@@ -1,10 +1,10 @@
 package ru.netology.honeybadger;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class Client {
@@ -12,19 +12,23 @@ public class Client {
     private final static String HOST = "netology.homework";
 
     public static void main(String[] args) {
-        try (Socket clientSocket = new Socket(HOST, PORT);
-             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        InetSocketAddress socketAddress = new InetSocketAddress(HOST, PORT);
+        try (final SocketChannel socketChannel = SocketChannel.open();
              Scanner scanner = new Scanner(System.in)) {
+            socketChannel.connect(socketAddress);
+            final ByteBuffer inputBuffer = ByteBuffer.allocate(2 << 10);
             String msg;
             while (true) {
                 System.out.println("(end-exit)Введите целое цисло:");
                 msg = scanner.nextLine();
-                out.println(msg);
                 if (msg.equals("end")) break;
-                System.out.println("Server: " + in.readLine());
+                socketChannel.write(ByteBuffer.wrap(msg.getBytes(StandardCharsets.UTF_8)));
+                Thread.sleep(2000);
+                int byreCount = socketChannel.read(inputBuffer);
+                System.out.println(new String(inputBuffer.array(), 0, byreCount, StandardCharsets.UTF_8));
+                inputBuffer.clear();
             }
-        } catch (IOException e) {
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
     }
--- src/ru/netology/honeybadger/Server.java
+++ src/ru/netology/honeybadger/Server.java
@@ -1,37 +1,48 @@
 package ru.netology.honeybadger;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.math.BigInteger;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+
+/*
+В данной задаче выбрал NonBlocking, так как пользователь может продолжительное время набирать большой
+текст и передавать его каналу, а тот складывать данные в буфер, в это время, чтобы не простаивать сервер может заниматься
+другими задачами, после обратиться к каналу, чтобы тот проверил если там какие-то данные для обрботки в буфере, если есть
+сервер получает, обратывает и передает каналу, который отправляет их в буфер для клиента.
+ */
 
 public class Server {
     private final static int PORT = 56432;
+    private final static String HOST = "netology.homework";
 
     public static void main(String[] args) {
         try {
-            ServerSocket serverSocket = new ServerSocket(PORT);
+            final ServerSocketChannel SOCKET_CHANNEL = ServerSocketChannel.open();
+            SOCKET_CHANNEL.bind(new InetSocketAddress(HOST, PORT));
             while (true) {
-                try (Socket socket = serverSocket.accept();
-                     PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                     BufferedReader in = new BufferedReader((new InputStreamReader(socket.getInputStream())))) {
-                    String line;
-                    while ((line = in.readLine()) != null) {
-                        out.println(getFibonacciValue(new BigInteger(line)));
-                        if (line.equals("end")) break;
+                try (SocketChannel socketChannel = SOCKET_CHANNEL.accept()) {
+                    final ByteBuffer inputBuffer = ByteBuffer.allocate(2 << 10);
+
+                    while (socketChannel.isConnected()) {
+                        int byteCount = socketChannel.read(inputBuffer);
+                        if (byteCount == -1) break;
+                        final String msg = new String(inputBuffer.array(), 0, byteCount, StandardCharsets.UTF_8);
+                        if (msg.equals("end")) break;
+                        inputBuffer.clear();
+                        socketChannel.write(ByteBuffer.wrap(("\nРезультат вычисления: " + getFibonacciValue(new BigInteger(msg))).getBytes(StandardCharsets.UTF_8)));
                     }
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    System.out.println(e.getMessage());
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
     public static BigInteger getFibonacciValue(BigInteger n) {
         if (n.compareTo(new BigInteger("2")) == -1) {
             return new BigInteger("0");
